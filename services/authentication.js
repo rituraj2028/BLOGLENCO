00aae9136150d41bada26af5073db5f86caaaad7
Rituraj Singh Rathore <123093046+rituraj2028@users.noreply.github.com>
2024-02-20 18:37:03 +0530

    working

---
+++ services/authentication.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const JWT = require("jsonwebtoken");
+
+function createTokenForUser(user) {
+  const payload = {
+    _id: user._id,
+    email: user.email,
+    profileImageURL: user.profileImageURL,
+    role: user.role,
+  };
+  const token = JWT.sign(payload, SECRET);
+  return token;
+}
+
+function validateToken(token) {
+  const payload = JWT.verify(token, secret);
+  return payload;
+}
+
+module.exports = {
+  createTokenForUser,
+  validateToken,
+};
